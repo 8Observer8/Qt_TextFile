1cf020f618fc41110570f521c5a6245295a40aff
Enzhaev Ivan <8observer8@gmail.com>
2014-07-01 20:10:29 +0400

    First Commit

--- main.cpp
+++ main.cpp
@@ -12,64 +12,29 @@ int main( )
     persons.push_back( david );
     persons.push_back( ivan );
 
-    // Parse the person array to the string content
-    QString content;
     try {
+        // Parse the person array to the string content
+        QString content;
         parsePersonsToStrContent( persons, content );
-    } catch( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
 
-    // Save the string content to the file
-    QString fileName = "file.txt";
-    try {
+        // Save the string content to the file
+        QString fileName = "file.txt";
         writeData( fileName, content );
+
+        // Read the string content from the file
+        QString readContent;
+        readData( fileName, readContent );
+
+        // Parse the string content to the person array
+        std::vector<Person> readPersons;
+        parseContentToPersons( readContent, readPersons );
+
+        // Print the person array on the screen
+        printData( readPersons );
     } catch ( const LogicError &e ) {
         std::cerr << e.what( ) << std::endl;
         return 1;
     } catch ( const FileError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
-
-    // Read the string content from the file
-    QString readContent;
-    try {
-        readData( fileName, readContent );
-    } catch ( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( const FileError &e) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
-
-    // Parse the string content to the person array
-    std::vector<Person> readPersons;
-    try {
-        parseContentToPersons( readContent, readPersons );
-    } catch( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
-
-    // Print the person array on the screen
-    try {
-        printData( readPersons );
-    } catch( const LogicError &e ) {
         std::cerr << e.what( ) << std::endl;
         return 1;
     } catch ( ... ) {
